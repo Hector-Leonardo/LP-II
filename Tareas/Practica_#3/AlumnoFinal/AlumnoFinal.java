2e26937cc4173a2124fdb8f928449c080cdf19a6
Hector-Leonardo <132015462+Hector-Leonardo@users.noreply.github.com>
2023-09-22 10:13:09 -0600

    Update AlumnoFinal.java

--- Tareas/Practica_#3/AlumnoFinal/AlumnoFinal.java
+++ Tareas/Practica_#3/AlumnoFinal/AlumnoFinal.java
@@ -1,3 +1,4 @@
+/*
 package AlumnoFinal;
 public class AlumnoFinal {
 public static void main (String[] args){
@@ -7,5 +8,50 @@ public static void main (String[] args){
         Alumno Alumno01 = new Alumno("Hector","Ingeniero en Sistemas Computacionales", 64374,3, 'B',tutor01); 
         String telefono = Alumno01.getTutor().getTelefono().toString();
         System.out.println("El telefono del Tutor es: "+telefono);
+    }
+}*/
+
+//Coreccion
+
+package AlumnoFinal;
+public class AlumnoFinal {
+public static void main (String[] args){
+        
+        Telefono numeroTutorPichis = new Telefono("+52","981","9818278440");
+        Tutor tutorPichis = new Tutor("Margarita", 56, numeroTutorPichis);
+        Alumno Pichis = new Alumno("Hector",
+        "Ingeniero en Sistemas Computacionales",
+        64374,3, 'B',tutorPichis); 
+        
+        System.out.println("----------------------------------------------------");
+        System.out.println("DATOS GENERALES DEL ALUMNO");
+        
+        String Nombre = Pichis.getNombre();
+        System.out.println("Nombre:"+Nombre);
+        
+        String Carrera = Pichis.getCarrera();
+        System.out.println("Carrera: "+Carrera);
+        
+        int Matricula = Pichis.getMatricula();
+        System.out.println("Matricula: "+Matricula);
+        
+        int Grado = Pichis.getGrado();
+        System.out.println("Grado:"+Grado);
+                
+        char Grupo = Pichis.getGrupo();
+        System.out.println("Grupo:"+Grupo);
+        
+        System.out.println("----------------------------------------------------");
+        System.out.println("DATOS DEL TUTOR");
+        
+        String NombreTutor = Pichis.getTutor().getNombre();
+        System.out.println("Nombre del Tutor: "+NombreTutor);
+        
+        int EdadTutor = Pichis.getTutor().getEdad();
+        System.out.println("Edad del Tutor: "+EdadTutor);
+               
+        String telefono = Pichis.getTutor().getTelefono().toString();
+        System.out.println("El telefono del Tutor es: "+telefono);
+        
     }
 }
