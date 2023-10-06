4e27c94dac1acfe75357fb5a4692bf3289ed802a
Hector-Leonardo <132015462+Hector-Leonardo@users.noreply.github.com>
2023-10-06 09:08:46 -0600

    Add files via upload

---
+++ Tareas/Pokemon_Herencia/BatallasdePokemones.java
@@ -0,0 +1,14 @@
+
+package PokemonBatallas;
+
+public class BatallasdePokemones {
+public static void main (String[] args) {
+    
+    PokemonTierra Cubone = new PokemonTierra (10, "Tipo Tierra");
+    PokemonFuego Charmander = new PokemonFuego (10, "Tipo Fuego");
+    
+    Cubone.atacar("Bone Club", Charmander);
+    Charmander.atacar("Ascuas Ember", Cubone);
+    
+    }    
+}
