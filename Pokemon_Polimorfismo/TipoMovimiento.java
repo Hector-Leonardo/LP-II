e9d501092873967d96a95f3185760aa1e9655e23
Hector-Leonardo <132015462+Hector-Leonardo@users.noreply.github.com>
2023-10-27 23:04:58 -0600

    Create TipoMovimiento.java

---
+++ Pokemon_Polimorfismo/TipoMovimiento.java
@@ -0,0 +1,11 @@
+public class TipoMovimiento {
+
+    private Tipo clase1;
+    private Tipo clase2;
+    private Tipo clase3;
+
+    public TipoMovimiento(Tipo clase1, Tipo clase2, Tipo clase3) {
+        this.clase1 = clase1;
+        this.clase2 = clase2;
+        this.clase3 = clase3;
+    }}
